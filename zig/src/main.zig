7a67e99573851cd8746d536ffb9ca73b0ccf069d
Jeremy Cowgar <jeremy@cowgar.com>
2022-05-21 18:59:20 -0400

    zig: now working
    not yet using module, documentation or unit testing

--- zig/src/main.zig
+++ zig/src/main.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const argsParser = @import("args");
 
 const Stats = struct {
     lines: u32,
@@ -6,36 +7,104 @@ const Stats = struct {
     chars: u32,
 };
 
-pub fn main() anyerror!void {
-    const stdout = std.io.getStdOut().writer();
+var showLines: bool = false;
+var showWords: bool = false;
+var showChars: bool = false;
 
-    var f = try std.fs.cwd().openFile("../testdata/mobydick.txt", .{});
-    var s = try count_lines(f);
-    try stdout.print(" {d} {d} {d}!\n", .{ s.lines, s.words, s.chars });
+const stdout = std.io.getStdOut().writer();
+
+pub fn main() anyerror!void {
+    var arena_instance = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena_instance.deinit();
+    const arena = arena_instance.allocator();
+
+    var files = std.ArrayList([]u8).init(arena);
+    defer files.deinit();
+
+    var args = try std.process.argsAlloc(arena);
+    var argCount: u32 = 0;
+
+    for (args) |arg| {
+        if (std.mem.eql(u8, arg, "-l")) {
+            showLines = true;
+        } else if (std.mem.eql(u8, arg, "-w")) {
+            showWords = true;
+        } else if (std.mem.eql(u8, arg, "-m")) {
+            showChars = true;
+        } else if (argCount > 0) {
+            try files.append(arg);
+        }
+
+        argCount += 1;
+    }
+
+    if (!showLines and !showWords and !showChars) {
+        showLines = true;
+        showWords = true;
+        showChars = true;
+    }
+
+    var totals = Stats{ .lines = 0, .words = 0, .chars = 0 };
+
+    for (files.items) |fname| {
+        var f = try std.fs.cwd().openFile(fname, .{});
+        var s = try count_lines(f);
+
+        try report(s, fname);
+
+        totals.lines += s.lines;
+        totals.words += s.words;
+        totals.chars += s.chars;
+    }
+
+    if (files.items.len > 1) {
+        try report(totals, "total");
+    }
+}
+
+pub fn report(s: Stats, label: []const u8) !void {
+    if (showLines) {
+        try stdout.print(" {d}", .{s.lines});
+    }
+
+    if (showWords) {
+        try stdout.print(" {d}", .{s.words});
+    }
+
+    if (showChars) {
+        try stdout.print(" {d}", .{s.chars});
+    }
+
+    try stdout.print(" {s}\n", .{label});
 }
 
 pub fn count_lines(f: std.fs.File) !Stats {
-    var r2 = f.reader();
-    var buf = std.io.bufferedReader(r2);
+    var buf = std.io.bufferedReader(f.reader());
     var r = buf.reader();
     var b: u8 = 1;
     var s = Stats{ .lines = 0, .words = 0, .chars = 0 };
+    var inWord = false;
 
     while (true) {
+        // TODO handle any non-endOfStream errors
         b = r.readByte() catch {
             break;
         };
 
         if (b == '\n') {
+            inWord = false;
             s.lines += 1;
+        } else if ((b >= 'a' and b <= 'z') or (b >= 'A' and b <= 'Z')) {
+            if (!inWord) {
+                inWord = true;
+                s.words += 1;
+            }
+        } else {
+            inWord = false;
         }
 
         s.chars += 1;
     }
 
     return s;
-}
-
-test "basic test" {
-    try std.testing.expectEqual(10, 3 + 7);
 }
