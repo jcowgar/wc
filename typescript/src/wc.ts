77c8bb92dcc007ff6b82ec3925c415dd4f132ae4
Jeremy Cowgar <jeremy@cowgar.com>
2022-05-22 09:08:48 -0400

    word count is working

--- typescript/src/wc.ts
+++ typescript/src/wc.ts
@@ -1,4 +1,4 @@
-import * as fs from 'fs/promises'
+import * as fs from 'fs'
 
 class Stats {
 	public lines: number
@@ -12,8 +12,42 @@ class Stats {
 	}
 }
 
-async function main() { }
+async function main() {
+	console.log(await word_count('../testdata/mobydick.txt'))
+}
 
-function word_count(fname: string) { }
+function word_count(fname: string): Promise<Stats> {
+	return new Promise((res, rej) => {
+		const stats = new Stats()
+		let inWord = false;
+
+		const rs = fs.createReadStream(fname)
+		rs.on('error', rej)
+		rs.on('data', (chunk) => {
+			const s = chunk.toString()
+
+			for (let i = 0; i < s.length; i++) {
+				const ch = s[i];
+
+				stats.chars++
+
+				if (ch >= 'A' && ch <= 'Z' || ch >= 'a' && ch <= 'z') {
+					if (!inWord) {
+						inWord = true
+						stats.words++
+					}
+				} else if (ch == '\n') {
+					inWord = false
+					stats.lines++
+				} else {
+					inWord = false
+				}
+			}
+		})
+		rs.on('end', () => {
+			res(stats)
+		})
+	})
+}
 
 main()
