b358c3f3025db055cd645d5edbe6bd5ed4f9701e
Jeremy Cowgar <jeremy@cowgar.com>
2022-05-22 08:55:44 -0400

    project setup

---
+++ typescript/src/wc.ts
@@ -0,0 +1,19 @@
+import * as fs from 'fs/promises'
+
+class Stats {
+	public lines: number
+	public words: number
+	public chars: number
+
+	constructor() {
+		this.lines = 0
+		this.words = 0
+		this.chars = 0
+	}
+}
+
+async function main() { }
+
+function word_count(fname: string) { }
+
+main()
